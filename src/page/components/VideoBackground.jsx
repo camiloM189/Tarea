9dd3d36a9b92e6f17ed852c3531602d4b0c63335
camiloM189 <jcamilomillan18912@gmail.com>
2024-10-16 16:28:24 -0500

    cambios en el video

--- src/page/components/VideoBackground.jsx
+++ src/page/components/VideoBackground.jsx
@@ -3,7 +3,7 @@ export const VideoBackground = () => {
              
         <div className="video-background">
         <video autoPlay loop muted>
-            <source src="/public/video.mp4" type="video/mp4" />
+            <source src="/video.mp4" type="video/mp4" />
             Tu navegador no soporta videos HTML5.
         </video>
         <div className="overlay" />
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -48,19 +48,19 @@ export const Home = () => {
         </div>
 
  
-        <section className="row g-0 mb-5 mt-5" style={{display:'flex',justifyContent:'center'}} >
+        {/* <section className="row g-0 mb-5 mt-5" style={{display:'flex',justifyContent:'center'}} >
      	
       
 
             <article className=" ms-3 mb-1" style={{cursor:'pointer',overflow:'hidden',width:"30%"}} onClick={scrollToDesarrollo}>
-             {/* <Link to={`/DesarrolloPage`}> */}
+          
                 <div className="card text-bg-dark border-0  rounded-0">
                   <img src="/desarrollo.jpg" className="card-img" alt="cliente1" style={{height: '280px',objectFit:'cover'}}/>
                   <div className="card-img-overlay second-alpha-color d-flex flex-column justify-content-md-center" style={{justifyContent:'center'}}>
                     <h5 className="card-title text-center">Desarrollo De Sotware</h5>
                   </div>
                 </div>
-             {/* </Link> */}
+        
             </article>	
 
             <article className="ms-3" style={{cursor:'pointer',overflow:'hidden',width:"30%"}} onClick={scrollToLenguaje}>
@@ -83,10 +83,45 @@ export const Home = () => {
             </article>	
 
 
-        </section>
+        </section> */}
          
+        <section className="row g-0 mb-5 mt-5" style={{display:'flex',justifyContent:'center'}} >
+     	
+      
+
+         <article className=" ms-3 mb-1 col-sm-12 col-6-md botones" style={{cursor:'pointer',overflow:'hidden'}} onClick={scrollToDesarrollo}>
+       
+             <div className="card text-bg-dark border-0  rounded-0">
+               <img src="/desarrollo.jpg" className="card-img" alt="cliente1" style={{height: '280px',objectFit:'cover'}}/>
+               <div className="card-img-overlay second-alpha-color d-flex flex-column justify-content-md-center" style={{justifyContent:'center'}}>
+                 <h5 className="card-title text-center">Desarrollo De Sotware</h5>
+               </div>
+             </div>
+     
+         </article>	
+
+         <article className="ms-3 col-sm-12 col-6-md botones" style={{cursor:'pointer',overflow:'hidden'}} onClick={scrollToLenguaje}>
+             <div className="card text-bg-dark border-0  rounded-0">
+               <img src="/lenguaje.jpg" className="card-img" alt="cliente1" style={{height: '280px',objectFit:'cover'}}/>
+               <div className="card-img-overlay second-alpha-color d-flex flex-column justify-content-md-center" style={{justifyContent:'center'}}>
+                 <h5 className="card-title text-center">Lenguaje De Programacion</h5>
+               </div>
+             </div>
+         </article>	
+
+ 
+         <article className="ms-3 col-sm-12 col-6-md botones" style={{cursor:'pointer',overflow:'hidden'}} onClick={scrollToNormas}>
+             <div className="card text-bg-dark border-0  rounded-0">
+               <img src="/normas.jpg" className="card-img" alt="cliente1" style={{height: '280px',objectFit:'cover'}}/>
+               <div className="card-img-overlay second-alpha-color d-flex flex-column justify-content-md-center" style={{justifyContent:'center'}}>
+                 <h5 className="card-title text-center">Normas</h5>
+               </div>
+             </div>
+         </article>	
 
 
+     </section>
+      
 
 
 
